263bb8dc63c8b8aa5ee2e0736620130ba75a2151
Ilya Zhidkov <ilya@youweb.su>
2015-09-02 12:18:04 +0600

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -358,12 +358,12 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+$config['sess_save_path'] = 'ci_sessions';
+$config['sess_expiration'] = 172800;
+$config['sess_match_ip'] = TRUE;
+$config['sess_time_to_update'] = 0;// $config['sess_expiration'];
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
@@ -413,7 +413,7 @@ $config['standardize_newlines'] = FALSE;
 |          for backwards compatibility purposes!
 |
 */
-$config['global_xss_filtering'] = FALSE;
+$config['global_xss_filtering'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,6 +5,8 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
+		$data_session_set = array('logged' => 1, 'username' => 'admin');
+		$this->session->set_userdata($data_session_set);
 		$this->load->view('welcome_message');
 	}
 }
