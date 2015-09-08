6f49966be9e3393a554c3570974b92c779bbb676
Ilya Zhidkov <ilya@youweb.su>
2015-09-08 13:04:34 +0600

    add

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -9,6 +9,7 @@ class Login extends Admin_Controller
     public function index()
     {
         $data = [];
+        show_error('ошибка', 500);
         $data['title'] = 'Login page';
         $this->load->view('main', $data);
     }
--- application/views/errors/html/error_general.php
+++ application/views/errors/html/error_general.php
@@ -1,64 +1,68 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+?>
+<!DOCTYPE html>
+<html class="error-page" lang="ru">
 <head>
-<meta charset="utf-8">
-<title>Error</title>
-<style type="text/css">
+    <meta charset="utf-8">
+    <title>Ошибка</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+    <!-- Bootstrap -->
+    <link rel="stylesheet" media="screen" href="/errors/css/bootstrap.min.css">
+    <link rel="stylesheet" media="screen" href="/errors/css/bootstrap-theme.min.css">
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+    <!-- Bootstrap Admin Theme -->
+    <link rel="stylesheet" media="screen" href="/errors/css/bootstrap-admin-theme.css">
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+    <!-- Bootstrap Error Page -->
+    <link rel="stylesheet" media="screen" href="/errors/css/bootstrap-error-page.css">
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="/errors/js/html5shiv.js"></script>
+    <script type="text/javascript" src="/errors/js/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+<!-- content -->
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="col-lg-12">
+            <div class="centering text-center error-container">
+                <div class="text-center">
+                    <h2 class="without-margin text-primary"><?php echo $heading; ?></h2>
+                    <h4 class="text-primary"></h4>
+                </div>
+                <div class="text-center">
+                    <blockquote>
+                        <p><?php echo $message; ?></p>
+                    </blockquote>
+                    <h3><small>Выберите опцию ниже</small></h3>
+                </div>
+                <hr>
+                <ul class="pager">
+                    <li><a href="javascript:history.back()">Назад</a></li>
+                    <li><a href="/">На главную</a></li>
+                    <!-- <li><a href="error-pages.html">Other error pages &rarr;</a></li> -->
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- footer -->
+<div class="navbar navbar-footer navbar-fixed-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <footer role="contentinfo">
+                    <p class="left"></p>
+                    <p class="right">&copy; 2015 <a href="http://xkelxmc.youweb.su" target="_blank">xkelxmc</a></p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
